d936518527be6a8475597f384d265e300aada378
LucasReisV1337 <lucasreisvasconcelos@icloud.com>
2022-12-29 09:39:31 -0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { ThemeProvider } from 'styled-components'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
